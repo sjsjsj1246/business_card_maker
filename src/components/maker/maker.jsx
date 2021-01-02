76e2de4cacf008d8e20dab2d7582535e072e2325
Hwang In Seo <sjsjsj1246@gmail.com>
2021-01-02 15:46:48 +0900

    ADD card and edit grom component

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 import Editor from '../editor/editor';
 import Footer from '../footer/footer';
@@ -7,6 +7,41 @@ import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
 const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: '1',
+      name: 'Hwang In Seo',
+      company: '3sec',
+      theme: 'light',
+      title: 'Software Engineer',
+      emain: 'sjsjsj1246@gmail.com',
+      message: 'hi',
+      fileName: 'sjsjsj1246',
+      fileRUL: '',
+    },
+    {
+      id: '2',
+      name: 'Hwang In Seo2',
+      company: '3sec',
+      theme: 'dark',
+      title: 'Software Engineer',
+      emain: 'sjsjsj1246@gmail.com',
+      message: 'hi',
+      fileName: 'sjsjsj1246',
+      fileRUL: '',
+    },
+    {
+      id: '3',
+      name: 'Hwang In Seo3',
+      company: '3sec',
+      theme: 'colorful',
+      title: 'Software Engineer',
+      emain: 'sjsjsj1246@gmail.com',
+      message: 'hi',
+      fileName: 'sjsjsj1246',
+      fileRUL: '',
+    },
+  ]);
   const history = useHistory();
   const onLogout = () => {
     authService.logout();
@@ -24,8 +59,8 @@ const Maker = ({ authService }) => {
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards} />
+        <Preview cards={cards} />
       </div>
       <Footer />
     </section>
