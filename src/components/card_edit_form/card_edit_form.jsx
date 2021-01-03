f37d39e53621305303cfb9e8c510141388fa3e1f
Hwang In Seo <sjsjsj1246@gmail.com>
2021-01-03 13:26:53 +0900

    Reform code

--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -9,16 +9,7 @@ const CardEditForm = ({ FileInput, card, updateCard, deleteCard }) => {
   const titleRef = useRef();
   const emailRef = useRef();
   const messageRef = useRef();
-  const {
-    name,
-    company,
-    title,
-    email,
-    message,
-    theme,
-    fileName,
-    fileURL,
-  } = card;
+  const { name, company, title, email, message, theme, fileName } = card;
 
   const onFileChange = (file) => {
     updateCard({
